334c07812a7f46a0deea44a925b0517b3620bb9b
Chanjong Baek <readandaction@gmail.com>
2020-09-07 11:48:20 +0900

    Create Havior Model

--- haviors/admin.py
+++ haviors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Havior)
+class HaviorAdmin(admin.ModelAdmin):
+    pass
--- haviors/models.py
+++ haviors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Havior(models.Model):
+
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField(blank=True)
+    bibles = models.ManyToManyField("bibles.Bible", related_name="haviors")
+    user = models.ForeignKey("users.User", related_name="haviors")
