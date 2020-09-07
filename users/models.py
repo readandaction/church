c5669504025e69f069d6b321d53ea2e3eef08b0c
Chanjong Baek <readandaction@gmail.com>
2020-09-07 10:58:57 +0900

    Create User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"))
+
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
