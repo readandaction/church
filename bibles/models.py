2ba29d2d39144c53ad1a72dcb8deabb88286e99b
Chanjong Baek <readandaction@gmail.com>
2020-09-07 11:41:08 +0900

    Create Bible Model

--- bibles/models.py
+++ bibles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Bible(models.Model):
+
+    location = models.CharField(max_length=10)
+    words = models.TextField()
+    description = models.TextField()
